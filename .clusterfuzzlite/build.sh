14628d814585761111e357298c9da2ee7f5ff1cb
Seongho Bae <me@seonghobae.me>
2026-03-26 14:16:45 +0900

    build: pin local pip install in clusterfuzzlite to resolve scorecard PinnedDependenciesID (#34)

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
-python3 -m pip install .
 python3 -m pip install --require-hashes -r .clusterfuzzlite/requirements.txt
+python3 -m pip install --no-deps -e .
+
 # compile fuzzer
 compile_python_fuzzer "$SRC/vector-topic-modeling/tests/fuzz_sanitize.py"
