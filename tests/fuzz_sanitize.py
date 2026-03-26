bcde141a50e0424b1d5236f509f43dded8640137
Seongho Bae <me@seonghobae.me>
2026-03-26 11:03:16 +0900

    fix: address CodeRabbit review comments

--- tests/fuzz_sanitize.py
+++ tests/fuzz_sanitize.py
@@ -5,13 +5,12 @@ import atheris
 with atheris.instrument_imports():
     from vector_topic_modeling._sanitize import redact_pii_and_secrets
 
+
 def TestOneInput(data):
     fdp = atheris.FuzzedDataProvider(data)
-    try:
-        text = fdp.ConsumeUnicodeNoSurrogates(len(data))
-    except Exception:
-        return
+    text = fdp.ConsumeUnicodeNoSurrogates(len(data))
     redact_pii_and_secrets(text)
+
 
 atheris.Setup(sys.argv, TestOneInput)
 atheris.Fuzz()
